65591288e074fcd3f2ab7db0e667113537d533b2
yasinafridi1 <yaseenafridi10875@gmail.com>
2024-08-29 18:24:26 +0500

    added feature of all generated text to be displayed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,40 +2,50 @@ import { Component } from '@angular/core';
 import { VoiceRecognitionService } from './services/voice-control.service';
 import { allowedCommands } from './constant/voiceControls';
 
-interface EnteredCommand{
+interface EnteredCommand {
   text: string;
-  status:boolean
+  status: boolean;
 }
 
-interface ListedCommands{
+interface ListedCommands {
   text: string;
-  serial:number;
+  serial: number;
 }
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'voiceControl';
   isMicOn = false;
-  userCommands:EnteredCommand[] =[];
-  allowedCommand:ListedCommands[] = allowedCommands;
+  userCommands: EnteredCommand[] = [];
+  allowedCommand: ListedCommands[] = allowedCommands;
+  userSpeech ="";  // Array to store all spoken text
 
-  constructor(private voiceRecognitionService: VoiceRecognitionService) { }
+  constructor(private voiceRecognitionService: VoiceRecognitionService) {}
 
   ngOnInit() {
     this.voiceRecognitionService.init();
   }
-  handleMic(){
+
+  handleMic() {
     this.isMicOn = !this.isMicOn;
-    if(this.isMicOn){
+    if (this.isMicOn) {
       this.voiceRecognitionService.start();
-      this.voiceRecognitionService.speechInputForToolbar().subscribe((voiceCommand: any) => {
-          this.userCommands = [...this.userCommands,voiceCommand]
-      });
-    }else{
+      this.voiceRecognitionService
+        .speechInputForToolbar()
+        .subscribe((voiceCommand: any) => {
+          this.userCommands = [voiceCommand, ...this.userCommands];
+        });
+
+        this.voiceRecognitionService.originalSpeechInputFromUser().subscribe((voiceCommand: any) => {
+          console.log("voice command",voiceCommand);
+          this.userSpeech = this.userSpeech+" "+ voiceCommand.text;
+        })
+    } else {
       this.voiceRecognitionService.stop();
     }
   }
 }
+
